fb0ee0fbb74d403fa89204056c9d54abac155e83
Enovai <tiwo2906@colorado.edu>
2022-04-05 10:38:08 -0600

    n

--- week12-demo.js
+++ week12-demo.js
@@ -16,7 +16,7 @@ async function getQuote(){
     }
   }
 }
-const endpoint = 'https://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1';
+const endpoint = 'http://quotes.stormconsultancy.co.uk/random.json';
 const quoteButton = document.querySelector('#js-new-quote');
 
 quoteButton.addEventListener('click',getQuote);
