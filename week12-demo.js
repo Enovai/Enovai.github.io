72f52fa5a2797c48f1964899b3436af0200cda95
Enovai <tiwo2906@colorado.edu>
2022-04-05 10:29:47 -0600

    aa

---
+++ week12-demo.js
@@ -0,0 +1,22 @@
+// document.querySelector('button');
+
+// document.querySelector('.new-quote button');
+async function getQuote(){
+  console.log('quote button has been clicked');
+  try{
+    const response= await fetch(endpoint);
+    if(!response.ok){
+      throw.Error(response.statusText);
+    }
+    const json= await response.json();
+    console.log(json);
+    catch(err){
+      console.log(err);
+      alert('failed');
+    }
+  }
+}
+const endpoint = 'https://random-quote-generator.herokuapp.com/api/quotes/random';
+const quoteButton = document.querySelector('#js-new-quote');
+
+quoteButton.addEventListener('click',getQuote);
