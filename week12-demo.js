222e6f05fb7f67bf911e2cab6de5c37acf78fe19
Enovai <tiwo2906@colorado.edu>
2022-04-07 20:30:06 -0600

    aaa

--- week12-demo.js
+++ week12-demo.js
@@ -2,26 +2,25 @@
 
 // document.querySelector('.new-quote button');
 async function getQuote(){
-  console.log('quote button has been clicked');
+  console.log("quote button has been clicked");
   try{
     const response= await fetch(endpoint);
     if(!response.ok){
-      throw.Error(response.statusText);
+      throw Error(response.statusText);
     }
     const json= await response.json();
     console.log(json);
-    displayQuote(json.message);
-    catch(err){
-      console.log(err);
-      alert('failed');
-    }
+    displayQuote(json);
+  }catch(err){
+    console.log(err);
+    alert('Failed');
   }
 }
 function displayQuote(quote){
   const quoteText = document.querySelector('#js-quote-text');
-  quotetext.textContent=quote;
+  quoteText.textContent=quote;
 }
-const endpoint = 'http://quotes.stormconsultancy.co.uk/random.json';
+const endpoint = 'https://geek-jokes.sameerkumar.website/api';
 const quoteButton = document.querySelector('#js-new-quote');
 
 quoteButton.addEventListener('click',getQuote);
